8b72608fd25251829765328bb64b77c1bb4f6c75
Gina Al Halabi <ghina7alabi@gmail.com>
2020-01-21 22:41:28 +0300

    Added Files

---
+++ WhackAMole/Assets/Scripts/Hole.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    int minInterval = 2;
+    int maxInterval = 10;
+
+    public GameObject Mole;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", Random.Range(minInterval, maxInterval), Random.Range(minInterval, maxInterval));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(Mole, this.gameObject.transform.position, Quaternion.identity);
+    }
+}
---
+++ WhackAMole/Assets/Scripts/Mole.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+    public float aliveTime = 1;
+
+    public GameObject HitEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, aliveTime);
+    }
+
+    private void OnMouseDown()
+    {
+        Instantiate(HitEffect, this.gameObject.transform.position, Quaternion.identity);
+    }
+}
